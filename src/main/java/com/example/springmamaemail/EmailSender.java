6793ca900ac5de7b2b8420f9bc8dfbbb890f6b53
Wojanix <wojtek.burek@gmail.com>
2024-01-25 17:50:24 +0100

    WORKING SENDING!!

--- src/main/java/com/example/springmamaemail/EmailSender.java
+++ src/main/java/com/example/springmamaemail/EmailSender.java
@@ -1,4 +1,22 @@
 package com.example.springmamaemail;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmailSender {
+    @Autowired
+    private JavaMailSender mailSender;
+    public void sendEmail(String toEmail, String subject, String body){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("stronamp@gmail.com");
+        message.setTo(toEmail);
+        message.setSubject(subject);
+        message.setText("blabla");
+        mailSender.send(message);
+
+        System.out.println("Mail sent suck seed");
+    }
 }
--- src/main/java/com/example/springmamaemail/SpringMamaEmailApplication.java
+++ src/main/java/com/example/springmamaemail/SpringMamaEmailApplication.java
@@ -1,13 +1,22 @@
 package com.example.springmamaemail;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class SpringMamaEmailApplication {
+    @Autowired
+    private EmailSender senderService;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringMamaEmailApplication.class, args);
+    }
+    @EventListener(ApplicationReadyEvent.class)
+    public void sendMail(){
+        senderService.sendEmail("wojtek.burek@gmail.com", "TestBackEnd", "To jest test backend 25 stycznia");
     }
 
 }
